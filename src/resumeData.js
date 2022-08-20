ce705a9feb96b7b4099078633f05a505497c9d25
Ruchika Gosain <sharma.ruchika34@gmail.com>
2022-08-19 22:09:47 -0400

    add the files

--- src/resumeData.js
+++ src/resumeData.js
@@ -18,7 +18,7 @@ let resumeData = {
       ],
     "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
     "address":"Canada",
-    "website":"https://rbhatia46.github.io",
+    "website":"https://socialflyweb.com",
     "education":[
       {
         "UniversityName":"The LNM Insitute of Information Technology",
