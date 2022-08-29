2934a492b4378bd1a2b3f9eb05903ba73eb5b951
Ruchika Gosain <sharma.ruchika34@gmail.com>
2022-08-29 15:13:24 -0400

    update the resume

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,6 +1,6 @@
 let resumeData = {
     "imagebaseurl":"https://ruchi222.github.io/",
-    "name": "Ruchika Gosain",
+    "name": "Ruchika",
     "role": "Frontend Developer, React Developer and Digital Marketer",
     "linkedinId":"https://www.linkedin.com/in/ruchika-gosain/",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
